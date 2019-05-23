53b1976bf29998ab7f629c72a12bc2559bcf0c7b
CrappyCornbread <49293577+CrappyCornbread@users.noreply.github.com>
2019-05-23 15:56:20 -0400

    Update BotInfo.java

--- src/main/java/xenobot/main/BotInfo.java
+++ src/main/java/xenobot/main/BotInfo.java
@@ -2,6 +2,6 @@ package xenobot.main;
 
 public class BotInfo {
 
-    public static final String TOKEN = "NTU4NDUwNTcwMDc2MzU2NjM4.XNhcOg.7xYfKTmP_NN2IDrhmrgBMGUd_oE";
+    public static final String TOKEN = getToken();
     public static final String ACTIVITY = "Your Bank Accounts";
 }
